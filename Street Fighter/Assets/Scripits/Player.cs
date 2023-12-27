78c06bb1b902d77aec4ed8921a5bd135a3ead774
Vinicius Abraão <b.abraao@escolar.ifrn.edu.br>
2023-12-27 16:27:47 -0300

    Atualizaçoes

--- Street Fighter/Assets/Scripits/Player.cs
+++ Street Fighter/Assets/Scripits/Player.cs
@@ -1,48 +1,71 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
     public float forwardJumpForce;
-    
+    public float lateralForce;
+
     public GameObject hadouken;
     public Transform pontoDeTiro;
-    
+    public Player2 Player1;
+
     private bool forwardJump;
     private bool isJumping;
     private bool isAttacking;
+    private bool applyingLateralForce; // Variável para rastrear se a força lateral está sendo aplicada
     private Animator anim;
     private Rigidbody2D rig;
-    // Start is called before the first frame update
+
     void Start()
     {
         anim = GetComponent<Animator>();
         rig = GetComponent<Rigidbody2D>();
+        Player1 = FindObjectOfType<Player2>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
         Jump();
         ForwardJump();
-        
+
         if (isAttacking == false)
         {
             StartCoroutine("Atacar");
         }
+        LookAtPlayer();
     }
 
     void Move()
     {
         float movement = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
-        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movement = 1;
+        }
+        else
+        {
+            movement = 0;
+        }
+
+        // Adiciona a força lateral ao cálculo da velocidade apenas durante o forward jump
+        if (applyingLateralForce)
+        {
+            rig.velocity = new Vector2((movement * speed) + lateralForce, rig.velocity.y);
+        }
+        else
+        {
+            rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+        }
+
         if (movement > 0)
         {
             if (!isJumping)
@@ -50,11 +73,11 @@ public class Player : MonoBehaviour
                 anim.SetInteger("transition", 1);
             }
             transform.eulerAngles = new Vector3(0, 0, 0);
-        } 
-        
+        }
+
         if (movement < 0)
         {
-            if (!isJumping )
+            if (!isJumping)
             {
                 anim.SetInteger("transition", 1);
             }
@@ -66,14 +89,15 @@ public class Player : MonoBehaviour
             anim.SetInteger("transition", 0);
         }
     }
+
     void Jump()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            if(!isJumping)
+            if (!isJumping)
             {
                 anim.SetInteger("transition", 2);
-                rig.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 isJumping = true;
             }
         }
@@ -81,13 +105,15 @@ public class Player : MonoBehaviour
 
     private void ForwardJump()
     {
-        if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
         {
             if (!forwardJump)
             {
                 anim.SetInteger("transition", 3);
-                rig.AddForce(new Vector2(0,forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(lateralForce, 0), ForceMode2D.Impulse);
                 forwardJump = true;
+                applyingLateralForce = true; // Ativa a aplicação de força lateral
             }
         }
 
@@ -96,11 +122,14 @@ public class Player : MonoBehaviour
             if (!forwardJump)
             {
                 anim.SetInteger("transition", 3);
-                rig.AddForce(new Vector2(0,forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(-lateralForce, 0), ForceMode2D.Impulse);
                 forwardJump = true;
+                applyingLateralForce = true; // Ativa a aplicação de força lateral
             }
         }
     }
+
     private IEnumerator Atacar()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -127,15 +156,34 @@ public class Player : MonoBehaviour
             isAttacking = false;
             anim.SetBool("Hadouken", false);
         }
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-   {
-       if (collision.gameObject.layer == 3)
-       {
-           isJumping = false;
-           forwardJump = false;
-       }
-   }
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            isJumping = false;
+            forwardJump = false;
+            applyingLateralForce = false; // Desativa a aplicação de força lateral ao pousar
+        }
+    }
+    void LookAtPlayer()
+    {
+        if (Player1 != null)
+        {
+            Vector3 direction = Player1.transform.position - transform.position;
+        
+            // Verifica se o inimigo está à direita ou à esquerda
+            if (direction.x > 0)
+            {
+                // Se o jogador estiver à direita do jogador principal
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                // Se o jogador estiver à esquerda do jogador principal
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+    }
 }
--- Street Fighter/Assets/Scripits/Player2.cs
+++ Street Fighter/Assets/Scripits/Player2.cs
@@ -1,64 +1,86 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Player2 : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
     public float forwardJumpForce;
-    
+    public float lateralForce;
+
     public GameObject hadouken;
     public Transform pontoDeTiro;
-    
+    private Player player;
     private bool forwardJump;
     private bool isJumping;
     private bool isAttacking;
+    private bool applyingLateralForce; // Variável para rastrear se a força lateral está sendo aplicada
     private Animator anim;
     private Rigidbody2D rig;
-    // Start is called before the first frame update
+
     void Start()
     {
         anim = GetComponent<Animator>();
         rig = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
         Jump();
         ForwardJump();
-        
+
         if (isAttacking == false)
         {
             StartCoroutine("Atacar");
         }
+        LookAtPlayer();
     }
 
     void Move()
     {
         float movement = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
-        
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement = 1;
+        }
+        else
+        {
+            movement = 0;
+        }
+
+        // Adiciona a força lateral ao cálculo da velocidade apenas durante o forward jump
+        if (applyingLateralForce)
+        {
+            rig.velocity = new Vector2((movement * speed) + lateralForce * Mathf.Sign(rig.velocity.x), rig.velocity.y);
+        }
+        else
+        {
+            rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+        }
+
+        // Ajusta a escala para olhar na direção correta
         if (movement > 0)
         {
             if (!isJumping)
             {
                 anim.SetInteger("transition", 1);
             }
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        } 
-        
-        if (movement < 0)
+            //transform.localScale = new Vector3(1, 1, 1); // Mantém a escala normal
+        }
+        else if (movement < 0)
         {
-            if (!isJumping )
+            if (!isJumping)
             {
                 anim.SetInteger("transition", 1);
             }
-            transform.eulerAngles = new Vector3(0, 180, 0);
+            //transform.localScale = new Vector3(-1, 1, 1); // Inverte a escala para olhar para a esquerda
         }
 
         if (movement == 0 && !isJumping)
@@ -66,14 +88,15 @@ public class Player2 : MonoBehaviour
             anim.SetInteger("transition", 0);
         }
     }
+
     void Jump()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if(!isJumping)
+            if (!isJumping)
             {
                 anim.SetInteger("transition", 2);
-                rig.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 isJumping = true;
             }
         }
@@ -81,61 +104,86 @@ public class Player2 : MonoBehaviour
 
     private void ForwardJump()
     {
-        if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
         {
             if (!forwardJump)
             {
                 anim.SetInteger("transition", 3);
-                rig.AddForce(new Vector2(0,forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(lateralForce * Mathf.Sign(rig.velocity.x), 0), ForceMode2D.Impulse);
                 forwardJump = true;
+                applyingLateralForce = true; // Ativa a aplicação de força lateral
             }
         }
 
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
         {
             if (!forwardJump)
             {
                 anim.SetInteger("transition", 3);
-                rig.AddForce(new Vector2(0,forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0, forwardJumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(-lateralForce * Mathf.Sign(rig.velocity.x), 0), ForceMode2D.Impulse);
                 forwardJump = true;
+                applyingLateralForce = true; // Ativa a aplicação de força lateral
             }
         }
     }
+
     private IEnumerator Atacar()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             isAttacking = true;
-            anim.SetBool("lpunch", true);
+            anim.SetBool("murro", true);
             yield return new WaitForSecondsRealtime(0.2f);
             isAttacking = false;
-            anim.SetBool("lpunch", false);
+            anim.SetBool("murro", false);
         }
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.L))
         {
             isAttacking = true;
-            anim.SetBool("LMKick", true);
+            anim.SetBool("chute", true);
             yield return new WaitForSecondsRealtime(0.2f);
             isAttacking = false;
-            anim.SetBool("LMKick", false);
+            anim.SetBool("chute", false);
         }
-        if (Input.GetKey(KeyCode.F) && Input.GetKey(KeyCode.Y) && Input.GetKey(KeyCode.U))
+        if (Input.GetKey(KeyCode.K) && Input.GetKey(KeyCode.L) && Input.GetKey(KeyCode.P))
         {
             isAttacking = true;
-            anim.SetBool("Hadouken", true);
+            anim.SetBool("hadouken", true);
             yield return new WaitForSecondsRealtime(0.58f);
             isAttacking = false;
-            anim.SetBool("Hadouken", false);
+            anim.SetBool("hadouken", false);
         }
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-   {
-       if (collision.gameObject.layer == 3)
-       {
-           isJumping = false;
-           forwardJump = false;
-       }
-   }
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            isJumping = false;
+            forwardJump = false;
+            applyingLateralForce = false; // Desativa a aplicação de força lateral ao pousar
+        }
+    }
+    void LookAtPlayer()
+    {
+        if (player != null)
+        {
+            Vector3 direction = player.transform.position - transform.position;
+        
+            // Verifica se o inimigo está à direita ou à esquerda
+            if (direction.x > 0)
+            {
+                // Se o jogador estiver à direita do jogador principal
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                // Se o jogador estiver à esquerda do jogador principal
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+    }
+
 }
